04926d1a91132e8d25017228fa74587d3581101c
Miko Jimenez <jimenezmikog@gmail.com>
2021-01-30 10:52:11 -0500

    Project 1 Inital Commit

---
+++ Project 1/contact.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Project 1 - Contact</title>
+    <meta name="description" content="Project 1 - Contact">
+    <meta name="author" content="Miko Jimenez">
+
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+</body>
+
+</html>
---
+++ Project 1/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Project 1 - Home</title>
+    <meta name="description" content="Project 1 - Home">
+    <meta name="author" content="Miko Jimenez">
+
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+    <h1>Project 1</h1>
+</body>
+
+</html>
