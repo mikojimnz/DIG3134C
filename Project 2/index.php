7ba22b7496c653e56c5e1b5b83ea147c224270db
Miko Jimenez <jimenezmikog@gmail.com>
2021-02-24 11:41:32 -0500

    Project 2 initialize

---
+++ Project 2/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Project 2 - Home</title>
+    <meta name="description" content="Project 1 - Home">
+    <meta name="author" content="Miko Jimenez">
+
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+    <div class="content">
+        <header>
+            <h1>Project2 – </h1>
+            <hr>
+            <h2>Miko Jimenez</h2>
+        </header>
+        <nav>
+            <a href="./index.php">Home</a>
+        </nav>
+        <div class="body">
+        </div>
+    </div>
+</body>
+
+</html>
